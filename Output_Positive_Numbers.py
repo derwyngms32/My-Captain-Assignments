b8f2631b0937af24c03dc9f61ba5fe7e3350f625
derwyngms32 <77180161+derwyngms32@users.noreply.github.com>
2022-05-26 00:04:30 +0530

    Add files via upload

---
+++ Output_Positive_Numbers.py
@@ -0,0 +1,15 @@
+#Input List 1 
+
+list1 = [12, -7, 5, 64, -14]
+#Iteration using For loops
+for num1 in list1:
+    #Implementing an If condition
+    if num1 > 0:
+        print(num1)
+
+#Input List 2
+list2 = [12, 14, -95, 3]
+
+for num2 in list2:
+    if num2 > 0:
+        print(num2)
